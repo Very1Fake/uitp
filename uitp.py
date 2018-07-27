b3e9c92c9dbcd05e657b834c9212754cd7e22dad
Timur <timurminer@gmail.com>
2018-07-27 23:19:52 +0300

    Version 0.0.0pa1

---
+++ uitp.py
@@ -0,0 +1,20 @@
+# UiTP LightPixel copyright 2018 GNU GPL-3.0
+# v0.0.0pa1
+
+class Protocol():
+    def __init__(self):
+        pass
+
+    def getMsgFromString():
+        pass
+
+    def getMsgFromArray():
+        pass
+
+
+class Universal(Protocol):
+    def __init__(self):
+        pass
+    
+    def sendMsg(self, ip, port):
+        pass
