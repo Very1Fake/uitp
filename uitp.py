b68fc5bca236047877ad27c156fc9098aad951ba
Timur <timurminer@gmail.com>
2018-07-28 14:16:33 +0300

    Version 0.0.0pa2

--- uitp.py
+++ uitp.py
@@ -1,15 +1,17 @@
 # UiTP LightPixel copyright 2018 GNU GPL-3.0
-# v0.0.0pa1
+# v0.0.0pa2
 
 class Protocol():
     def __init__(self):
         pass
 
-    def getMsgFromString():
-        pass
+    def getMsgFromString(self, string, encoding='utf-8'):
+        msg = bytes(string, encoding)
+        return msg
 
-    def getMsgFromArray():
-        pass
+    def getMsgFromArray(self, array, encoding='utf-8'):
+        msg = bytearray(array, encoding)
+        return msg
 
 
 class Universal(Protocol):
